af792e3b039e8f02e7d3fe768e0b0f067b9c3b0c
Matthew Benton <m@mattb.tech>
2023-10-10 20:59:03 +0100

    More neon progress

--- README.md
+++ README.md
@@ -2,4 +2,19 @@
 
 ## Neon
 
-Set `NEON_TOKEN` to an API key from Neon
+Set `NEON_API_KEY` to an API key from Neon.
+
+### Database Password
+
+Currently there is an issue with the neon terraform provider returning all passwords as empty string: https://github.com/kislerdm/terraform-provider-neon/issues/51
+
+Accordingly use the following to manually fetch the password and manually update the Secret Manager secret:
+
+```
+curl --request GET \
+     --url https://console.neon.tech/api/v2/projects/$PROJECT_ID/branches/$BRANCH_ID/roles/mjwbenton/reveal_password \
+     --header 'accept: application/json' \
+     --header 'authorization: Bearer '$NEON_API_KEY
+```
+
+$PROJECT_ID and $BRANCH_ID are Terraform outputs from the `stablio-data` stack
--- packages/infrastructure/StablioDataStack.ts
+++ packages/infrastructure/StablioDataStack.ts
@@ -1,5 +1,7 @@
 import { Construct } from "constructs";
-import { S3Backend, TerraformStack } from "cdktf";
+import { S3Backend, TerraformStack, Fn, TerraformOutput } from "cdktf";
+import * as neon from "./.gen/providers/neon";
+import * as aws from "@cdktf/provider-aws";
 
 export class StablioDataStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
@@ -11,7 +13,45 @@ export class StablioDataStack extends TerraformStack {
       region: "us-east-1",
     });
 
+    new aws.provider.AwsProvider(this, "AWS", {
+      region: "eu-west-1",
+    });
 
-    new random.provider.RandomProvider(this, "random");
+    new neon.provider.NeonProvider(this, "Neon", {});
+
+    const neonProject = new neon.project.Project(this, "Project", {
+      name: "stablio",
+      regionId: "aws-eu-central-1",
+    });
+
+    const databaseSecret = new aws.secretsmanagerSecret.SecretsmanagerSecret(
+      this,
+      "Secret",
+      {
+        namePrefix: "stablio-data",
+      },
+    );
+
+    new aws.secretsmanagerSecretVersion.SecretsmanagerSecretVersion(
+      this,
+      "SecretVersion",
+      {
+        secretId: databaseSecret.id,
+        secretString: Fn.jsonencode({
+          host: neonProject.databaseHost,
+          user: neonProject.databaseUser,
+          password: neonProject.databasePassword,
+          db: neonProject.databaseName,
+        }),
+      },
+    );
+
+    new TerraformOutput(this, "ProjectId", {
+      value: neonProject.id,
+    });
+
+    new TerraformOutput(this, "BranchId", {
+      value: neonProject.defaultBranchId,
+    });
   }
 }
