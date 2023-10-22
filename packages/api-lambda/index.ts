e1914dc4038b458eeb39a9c8a562674d24b50609
Matthew Benton <m@mattb.tech>
2023-10-22 20:56:19 +0100

    Fetch highlights in API

--- packages/api-lambda/index.ts
+++ packages/api-lambda/index.ts
@@ -1,6 +1,34 @@
+import { db, book, highlight, eq } from "@mattb.tech/stablio-db";
+
 export async function handler(event: AWSLambda.APIGatewayProxyEventV2) {
+  const [_, pathBook, billioId] = event.rawPath.split("/");
+  if (pathBook != "book" || !billioId) {
+    return {
+      status: 404,
+      body: `{}`,
+    };
+  }
+  const result = await (await db)
+    .select()
+    .from(book)
+    .where(eq(book.billioId, billioId))
+    .innerJoin(highlight, eq(book.id, highlight.bookId));
+
+  if (!result.length) {
+    return {
+      status: 404,
+      body: `{}`,
+    };
+  }
+
   return {
     status: 200,
-    body: `{}`,
+    body: JSON.stringify({
+      billioId,
+      highlights: result.map(({ highlight: { location, text } }) => ({
+        location,
+        text,
+      })),
+    }),
   };
 }
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,3 +1,3 @@
 export * from "./db";
 export * from "./schema";
-export { sql } from "drizzle-orm";
+export { sql, eq } from "drizzle-orm";
